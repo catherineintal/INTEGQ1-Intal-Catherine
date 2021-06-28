3279e179b354b182d0e54be04d6fca670b0ec4e4
catherineintal <catherineintal18@gmail.com>
2021-06-28 14:48:59 +0700

    TimeKeepingSystem

--- TimeKeepingSystem/Program.cs
+++ TimeKeepingSystem/Program.cs
@@ -26,7 +26,7 @@ namespace TimeKeepingSystem
                 Console.WriteLine("");
             }
             else
-            {
+            { 
                 Console.WriteLine("666");
             }
 
